05725813fede9525c82bb11799cece31e30552ca
Genoxci <genoxci.dev@gmail.com>
2026-05-29 14:57:16 +0800

    giuliac: Update sepolicy related to haptic profile picker

--- vibrator/sepolicy/SEPolicy.mk
+++ vibrator/sepolicy/SEPolicy.mk
@@ -1,2 +1,8 @@
 BOARD_VENDOR_SEPOLICY_DIRS += \
      device/oneplus/giuliac/vibrator/sepolicy/vendor
+
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
+     device/oneplus/giuliac/vibrator/sepolicy/public
+
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+     device/oneplus/giuliac/vibrator/sepolicy/private
